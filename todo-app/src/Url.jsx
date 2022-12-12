94a81bc954e567f29f63fb08d2fa045503c70b70
Rohit <rohitdorage3@gmail.com>
2022-12-12 07:56:04 +0530

    updated

--- todo-app/src/Url.jsx
+++
@@ -1,1 +0,0 @@
-export const url = "https://alive-cow-spacesuit.cyclic.app";
--- todo-app/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import {legacy_createStore, applyMiddleware, combineReducers} from 'redux';
-import {reducer as AppReducer} from './AppReducer/reducer';
-import {reducers as AuthReducer} from './AuthReducer/reducers';
-import thunk from 'redux-thunk';
-
-const rootReducer = combineReducers({AppReducer, AuthReducer});
-
-
-export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
